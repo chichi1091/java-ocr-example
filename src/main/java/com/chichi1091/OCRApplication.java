9476264e20b621d864cc59fd481a13f0a10b4abe
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2019-12-07 23:08:44 +0900

    PDFも試してみる

--- src/main/java/com/chichi1091/OCRApplication.java
+++ src/main/java/com/chichi1091/OCRApplication.java
@@ -10,14 +10,15 @@ import java.nio.file.Paths;
 public class OCRApplication {
     public static void main(String[] args) {
         try {
-            final String path = "src/main/resources/images/sample2.png";
+//            final String path = "src/main/resources/images/sample2.png";
+            final String path = "src/main/resources/pdf/sample.pdf";
             File imageFile = Paths.get(path).toFile();
 
             ITesseract instance = new Tesseract();
             final String tessdataPath = "src/main/resources/tessdata";
             instance.setDatapath(Paths.get(tessdataPath).toAbsolutePath().toString());
-//            instance.setLanguage("jpn");        // 横書き
-            instance.setLanguage("jpn_vert");   // 縦書き
+            instance.setLanguage("jpn");        // 横書き
+//            instance.setLanguage("jpn_vert");   // 縦書き
 
             String result = instance.doOCR(imageFile);
             System.out.println(result);
